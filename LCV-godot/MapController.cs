693db00c20d31461c4dc4a4e12f1576892e907da
NetSalvage <toddsusser@outlook.com>
2022-08-20 18:54:45 -0400

    Spent way too long before realizing that I should just focus on making hex maps happen, rather than worrying about how hex maps look. This draws complete hexagons and I should be happy with that for now.

--- LCV-godot/MapController.cs
+++ LCV-godot/MapController.cs
@@ -11,30 +11,33 @@ public class MapController : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		lineDrawer = GetNode<Line2D>("HexLine"); //RE line10: like here
-
+		lineDrawer = GetNode<Line2D>("CellLine"); //RE line10: like here
+		//lineDrawer.EndCapMode = enum LineCapMode 1;
+		
 		//in all likelihood, the rest of this function will eventually be triggered by a Signal
 		//map size/etc info will go here
 		GD.Print("Printing the Hexagons");
 		//foreach hex etc.
 		Vector2 hexPos;
-		hexPos.x = 0;
-		hexPos.y = 0;
-		DrawHex(hexPos);
+		hexPos.x = 200;
+		hexPos.y = 200;
+		DrawHex(hexPos,30);
 		GD.Print("Done");
 	}
 	
-	public void DrawHex(Vector2 a)
+	public void DrawHex(Vector2 center, float size)
 	{
-		int i = 0;
-		int angle_deg = 60 * i;
-		float angle_rad = Mathf.Pi * angle_deg / 180;
-		/* hold on
-		for (i = 0; i < 5; i++) {
-			lineDrawer.set_points(i) = 
-		
-		}*/
-		GD.Print("Drew hex at ("+ a.x +","+ a.y +")");
+		int angleDeg; 
+		float angleRad;
+		Vector2 vertex;
+		for (int i = 0; i <= 6; i++) {
+			angleDeg = 60 * i;
+			angleRad = Mathf.Pi * angleDeg / 180;
+			vertex.x = center.x + size * Mathf.Cos(angleRad);
+			vertex.y = center.y + size * Mathf.Sin(angleRad);
+			lineDrawer.AddPoint(vertex, i);
+		}
+		GD.Print("Drew hex at ("+ center.x +","+ center.y +")");
 	}
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
