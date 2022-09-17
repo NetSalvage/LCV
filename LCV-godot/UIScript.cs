93719029f13c805645a3c21bcf8bdeed096e7609
NetSalvage <toddsusser@outlook.com>
2022-09-17 18:54:06 -0400

    More UI functionality. Can select multiple hexes. Can "back out" of selections by pressing Esc. Next time I work on this, I'll finally add in some actual hex math stuff.

--- LCV-godot/UIScript.cs
+++ LCV-godot/UIScript.cs
@@ -1,21 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class UIScript : Node2D
 {
 	Camera2D thisCam;
-	Label hexCoordsLabel;
 	CanvasLayer helpWindow;
+	Label hexCoordsLabel;
+	List<Vector2> selectedHex;
+	bool mouseInUI;
+	bool windowsOpen;
 	
 	[Signal]
 	public delegate void MapClicked (Vector2 mousePos);
-	
+		
 	public override void _Ready()
 	{
 		thisCam = GetNode<Camera2D>("UserCam");
 		thisCam.MakeCurrent();
 		hexCoordsLabel = GetNode<Label>("UserCam/UserCanvasLayer/BottomRightWindow/BottomRightLabel"); //whew, lol.
-		helpWindow = GetNode<CanvasLayer>("UserCam/UserCanvasLayer/HelpWindowScene");
+		helpWindow = GetNode<CanvasLayer>("UserCam/UserCanvasLayer/Windows/HelpWindowScene");
+		selectedHex = new List<Vector2>();
 	}
 
 	//things that should happen only on press
@@ -23,13 +28,24 @@ public class UIScript : Node2D
 	{
 		if (inputEvent.IsActionPressed("choose_0"))
 		{
-			//check for UI elements being pressed, first. If not:
-			this.EmitSignal("MapClicked", GetGlobalMousePosition());
+			if (mouseInUI==false && windowsOpen == false)
+			{
+				this.EmitSignal("MapClicked", GetGlobalMousePosition());
+			}
 		}
 		
 		if (inputEvent.IsActionPressed("ui_help"))
 		{
 			helpWindow.SetVisible(!helpWindow.IsVisible());
+			if (helpWindow.IsVisible())
+			{
+				windowsOpen = true;
+			}
+		}
+		
+		if (inputEvent.IsActionPressed("ui_cancel"))
+		{
+			BackOneLevel();
 		}
 
 	}
@@ -37,10 +53,10 @@ public class UIScript : Node2D
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		InputCheck(delta);
+		LongInputCheck(delta);
 	}
 	
-	void InputCheck(float delta)
+	void LongInputCheck(float delta)
 	{
 		//camera movement
 		if (Input.IsActionPressed("ui_left"))
@@ -64,7 +80,47 @@ public class UIScript : Node2D
 
 	void OnCoordsReceived (Vector2 hexCoords)
 	{
-		hexCoordsLabel.SetText("(" + hexCoords.x + "," + hexCoords.y + ")");
+		if (selectedHex.Count < 1)
+		{
+			selectedHex.Add(hexCoords);
+			hexCoordsLabel.SetText	("(" + selectedHex[0].x + "," + selectedHex[0].x + ")");
+		}
+		else if (selectedHex.Count < 2)
+		{
+			selectedHex.Add(hexCoords);
+			hexCoordsLabel.SetText	("(" + selectedHex[0].x + "," + selectedHex[0].x + ")" + '\n' 
+									+"(" + selectedHex[1].x + "," + selectedHex[1].x + ")");
+		}
+	}
+	
+	void BackOneLevel()
+	{
+		//i'm going to have to use a List to somehow record "actions taken" by the player, and then use this function to remove the latest step.
+		//later I'll incorporate BackOneStep too, for specific windows. Which will do substeps one at a time instead of stepping out of procedures.
+		while (selectedHex.Count != 0)
+		{
+			selectedHex.RemoveAt(0);
+		}
+		hexCoordsLabel.SetText	("Clicked hex coordinates go here.");
+	}
+
+	//These two functions are connected to various UI elements through Godot's interface, for my own sanity:
+	private void OnMouseInUI() 
+	{
+		mouseInUI = true;
+	}
+
+	private void OnMouseOutUI()
+	{
+		mouseInUI = false;
 	}
 
 }
+
+
+
+
+
+
+
+
