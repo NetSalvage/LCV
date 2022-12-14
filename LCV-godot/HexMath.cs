278ab392fce47c0b57b5c6e5103dc92f9a11b8c8
NetSalvage <toddsusser@outlook.com>
2022-12-14 18:24:21 -0500

    Work on line-drawing algorithm
    No new functionality yet, but everything has been planned and is now being implemented.

--- LCV-godot/HexMath.cs
+++ LCV-godot/HexMath.cs
@@ -1,6 +1,10 @@
 using Godot;
 
 namespace HexMath {
+	struct lineOddQ { //weirdly enough this will be the most straightforward way to do this
+		Vector2[] cell; //"start" will be vector2[0], "end" will be vector2[count-1]
+	}
+
     public static class OddQ {
 	/*	Namespaces are shortcuts, so OddQ is mostly shortcuts.
 		All math in HexMath is done in the "Cube" class, but users can input to OddQ.
@@ -29,6 +33,28 @@ namespace HexMath {
 		public static Vector2 Neighbor(Vector2 hex, int direction){
 			return(Coords(Cube.Neighbor(CubeCoords(hex),direction)));
 		}
+
+		public static Vector2[] Line(Vector2 start, Vector2 end, TileMap tileMap) {
+			//my logic makes this easiest with oddQ, so vector3 will be the "redirecting" function.
+			//FAR FUTURE: rewrite this to more naturally use a hex grid. Will probably need that TileMap supports hex grids.
+			Vector2[] lineOut = new Vector2 [(Cube.Distance(CubeCoords(start),CubeCoords(end)))];
+			//determine biases, then set prim and sec directions based on that.
+			Vector2 diff = end - start;
+			//handle potential edge cases
+
+			float angle = Mathf.Atan2(diff.y,diff.x); //this is in radians
+			int prim; //most common move direction. Integers match those in "Neighbor" function
+			int sec; //alternative move direction.
+
+			if (0 <= angle && angle < 30) { //either SE or NE, NE primary
+			}
+			else if (30 <= angle && angle < 60) { //either NE or N, NE primary
+			}
+			else if (60 <= angle && angle < 90) { //either NE or N, N primary
+			}
+
+			return(lineOut);
+		}
     }
 
 	public static class Cube {
@@ -54,7 +80,7 @@ namespace HexMath {
 				case 5:
 					return new Vector3(hex.x-1, hex.y, hex.z+1);
 				default:
-					GD.Print("ERROR: Nonexistent direction. 0 is north, 1 is northeast...original hex returned instead.");
+					GD.Print("ERROR: Nonexistent direction. 0 is \"up\", 1 is \"up-right\"...original hex returned instead.");
 					return hex;
 			}
 		}
@@ -79,6 +105,120 @@ namespace HexMath {
 						" Original hex returned instead.");
 					return hex;
 			}
+		}
+
+		public static Vector3[] Line(Vector3 start, Vector3 end, TileMap tileMap) {
+			/*teach yourself bresenham, then implement it:
+			https://zvold.blogspot.com/2010/01/bresenhams-line-drawing-algorithm-on_26.html
+			*/
+			//FAR FUTURE: rewrite this to more naturally use a hex grid. Will probably need that TileMap supports hex grids.
+			//Let's bias towards clockwise for "first moves" and "tiebreakers".
+			Vector3[] lineOut = new Vector3 [(Cube.Distance((start),(end)))];
+			//determine biases, then set prim and sec directions based on that.
+			Vector3 diff = end - start;
+			
+			//check for "perfect" directions
+			if (diff.x == 0) { //vertical
+				if (diff.y > 0) { //"r" is larger, which means it's going down
+					//fill in lineOut with each hex in that vertical line. +R, -S each time.
+				} else {
+					//fill in lineOut. -R, +S each time.
+				}
+			}
+			else if (diff.x == -diff.y) { //UR or DL
+				if (diff.x > 0) { //going "right", so, UR
+					//fill in lineOut. +Q, -R each time.
+				} else {
+					//fill in lineOut. -Q, +R each time.
+				}
+			}
+			else if (diff.x == -diff.z) { //DR or UL
+				if (diff.x > 0) { //going "right", so, DR
+					//fill in lineOut. +Q, -S each time.
+				} else {
+					//fill in lineOut. -Q, +S each time.
+				}
+			}
+			else if (diff.y == diff.z) { //L or R
+				if (diff.y < 0) { //both are negative, so moving "right"
+					//fill in lineOut. Alternate between -R and -S. +Q each time. 
+				} else {
+					//fill in lineOut. Alternate between +R and +S. -Q each time.
+				}
+			}
+			else { //All other cases go in here.
+				//Start by determining the Two Possible Movement Directions.
+				int prim; //most common move direction. Integers match those in "Neighbor" function
+				int sec; //alternative move direction.
+				if (diff.x > 0) { //it goes somewhat R.
+					if (diff.y > 0) { //Each move is either DR or D.
+						if (-2*diff.y <= diff.z){ //abs(r) is half or more of abs(s), which means the line is equal or more "vertical".
+							prim = 3;
+							sec = 2;
+						} else {
+							prim = 2;
+							sec = 3;
+						}
+					} else { //Each move is either UR or U.
+						if (-2*diff.y > diff.z){ //abs(r) is more than half of abs(s), which means the line is more "vertical".
+							prim = 0;
+							sec = 1;
+						} else {
+							prim = 1;
+							sec = 0;
+						}
+					}
+				} else { //it goes somewhat L.
+					if (diff.y > 0) { //Each move is either DL or D.
+						if (-2*diff.y < diff.z){ //abs(r) is more than half of abs(s), which means the line is more "vertical".
+							prim = 3;
+							sec = 4;
+						} else {
+							prim = 4;
+							sec = 3;
+						}
+					} else { //Each move is either UL or U.
+						if (-2*diff.y >= diff.z){ //abs(r) is half or more of abs(s), which means the line is equal or more "vertical".
+							prim = 0;
+							sec = 5;
+						} else {
+							prim = 5;
+							sec = 0;
+						}
+					}
+				}
+				//Now we can assemble the line.
+				lineOut[0] = start;
+				Vector2 startCartesian = tileMap.MapToWorld(OddQ.Coords(start));
+				Vector3 nextCell;
+				Vector2 nextVertex;
+				Vector2 diffCartesian = tileMap.MapToWorld(OddQ.Coords(diff));
+				for (int i = 1; i < lineOut.Length-1; i++) {
+					nextVertex = startCartesian + (diffCartesian*i/(lineOut.Length));
+					//compare prim vs sec, the one closest to nextVertex is the next cell
+					//CONTINUE HERE
+					//lineOut[i] = nextCell; 
+				}
+				lineOut[lineOut.Length] = end;
+				return lineOut;
+			}
+
+			/*if no perfect direction, we do this somewhere between redblob and bresenham:
+				-Draw a line in Cartesian space, from the center of the start hex to the center of the end hex.
+					More precisely, make a list of points, "line length" long,
+					where the start point is in the start hex, and the end point is in the end hex.  
+					We'll also be using the tilemap to get Cartesian coordinates for this.
+				-For each prospective hex in our line, we decide between two neighbors to move into.
+					We can determine which two neighbors to choose between based on
+					which "straight line" cases we're between (see above).
+				-Repeat until we have all hexes filled in.
+			*/
+
+			return(lineOut);
+
+
+			Vector3[] hexLine = new Vector3[3];
+			return hexLine;
 		}
 	}
 }
